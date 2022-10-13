3fead561166fb5cebca9c9b08e33ccd3cb964a5f
TosBam <samuelson.os19@gmail.com>
2022-10-13 13:44:11 -0700

    size of func

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,16 @@
  */
 int main(void)
 {
-	printf("Sizes of a char: %d byte(s)\n", sizeof(char));
-	printf("Sizes of a int: %d byte(s)\n", sizeof(int));
-	printf("Sizes of a lond int: %d byte(s)\n", sizeof(long int));
-	printf("Sizes of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Sizes of a float: %d byte(s)\n", sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Sizes of a char: %d byte(s)\n", sizeof(a));
+	printf("Sizes of a int: %d byte(s)\n", sizeof(b));
+	printf("Sizes of a lond int: %d byte(s)\n", sizeof(c));
+	printf("Sizes of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Sizes of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
