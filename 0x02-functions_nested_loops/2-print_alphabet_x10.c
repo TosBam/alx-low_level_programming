8c7c324e6df6502e4a5e3cf00540d672067bdf74
TosBam <samuelson.os19@gmail.com>
2022-10-18 13:06:22 -0700

    print alphabet in 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int count;
+	char d;
+
+	for (count = 0; count <= 9 count++)
+	{
+		for (d = 'a'; d <= 'z'; d++)
+			_putchar(d);
+
+		_putchar('\n');
+	}
+}
