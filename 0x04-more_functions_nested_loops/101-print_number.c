66bc2b0b491684f4bf850e5afe737abeefd6c3ee
TosBam <samuelson.os19@gmail.com>
2022-10-20 15:56:14 -0700

    print prime

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_number - print an interger
@@ -8,16 +10,27 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int x, y, count;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		num = -num;
+		_putchar(45);
+		x = -n;
 	}
+	else
+	{
+		x = n;
+	}
+	y = x;
+	count = 1;
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
-	_putchar((num % 10) + 48);
-
+	while (y > 9)
+	{
+		y /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((x / count) % 10) + 48);
+	}
 }
