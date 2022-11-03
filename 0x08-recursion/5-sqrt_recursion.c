f5610a6d7507b2de2f074822bf76f0c6770d5a85
TosBam <samuelson.os19@gmail.com>
2022-11-03 14:09:41 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,46 +1,44 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
- * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
- * Return: square root of @n or -1
+ * _evaluate - evaluate function sqrt
+ * @i: interger
+ * @n: interger
+ * Return: evaluate sqrt
  */
 
-int _sqrt_recursion_wrapper(int n, int min, int max)
+int _evaluate(int i, int n)
 {
-	int guess, guess_squared;
+	/*Evaluate function*/
+	if (n == 0 || n == 1)
+		return (n);
 
-	guess = (min + max) / 2;
-	guess_squared = guess * guess;
+	else if (i * i < n)
+		return (_evaluate(i + 1, n));
 
-	if (guess_squared == n)
-		return (guess);
-	else if (min == max)
-		return (-1);
-	else if (guess_squared < n)
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else
-		return (_sqrt_recursion_wrapper(n, guess - 1, min));
+	else if (i * i == n) /*Condiction base*/
+		return (i);
 
+	return (-1);
+
+	return (-1);
 }
 
 /**
- * _sqrt_recursion - a function that returns the natural
- * square root of a number
- * @n: input number
- * Return: square root
+ * _sqrt_recursion - evaluate sqrt
+ * @n: interger
+ * Return: Sgrt_recursion
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n == 1)
-		return (1);
-	else if (n == 0)
-		return (0);
-	else if (n < 0)
+	int i = 0;
+
+	if (i < 0) /*If n is negative*/
 		return (-1);
+
 	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+	{
+		return (_evaluate(i, n)); /*Recursive call*/
+	}
 }
