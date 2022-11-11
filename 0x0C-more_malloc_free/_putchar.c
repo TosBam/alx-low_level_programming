61b53c309e4ad9ff0daa7ece4ad656d54eb0c701
TosBam <samuelson.os19@gmail.com>
2022-11-10 22:13:38 -0800

    task 1 and 2

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ *
+ * _putchar - write the character c to stdout
+ * @c: char to print
+ * Return: 1 else -1
+ */
+
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
+}
