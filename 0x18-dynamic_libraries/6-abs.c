101264a15e488d1e4b36dce3b5fd1ffebfb9b5e4
TosBam <samuelson.os19@gmail.com>
2023-03-20 07:13:23 -0700

    My shared(dynamic) library

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _abs - A funtion that print absolute value of interger
+ * @n: the interger
+ * Return: 0
+ */
+
+int _abs(int n)
+
+{
+
+	if (n < 0)
+
+	{
+
+		return (n * (-1));
+
+	}
+
+
+	else if (n == 0)
+
+	{
+
+		return (0);
+
+	}
+
+	else
+
+	{
+
+		return (n);
+
+	}
+
+}
